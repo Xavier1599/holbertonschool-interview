ab4eec7d6c1f613d0ef370dcf6a65ae86c431b58
Xavier1599 <xavierperez1599@gmail.com>
2023-01-05 17:11:33 -0400

    adding project

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Function that gets min num of ops
+"""
+import sys
+
+def minOperations(n):
+
+    if (n < 2):
+        return 0
+    for i in range(n // 2, 0, -1):
+        if n % i == 0:
+            return n // i + minOperations(i)
+            
